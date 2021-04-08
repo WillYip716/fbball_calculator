55e6f80b140772507b9b1fbac1d75a451b5e0a2a
WillYip716 <william.yip.716@gmail.com>
2021-04-07 19:06:31 -0700

    table for players

--- frontend/react_app/src/components/home.js
+++ frontend/react_app/src/components/home.js
@@ -1,33 +1,111 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
+import BootstrapTable from 'react-bootstrap-table-next';
 import axios from 'axios';
- 
+import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css"
 
-function Home() {
-  const [data, setData] = useState([]);
- 
+class Home extends Component {
+  state = {
+    products: [],
+    columns: [
+      {
+        dataField: 'Player_Name',
+        text: 'Name',
+        sort: true
+      },
+      {
+        dataField: 'GP',
+        text: 'GP',
+        sort: true
+      },
+      {
+        dataField: 'PTS',
+        text: 'PTS',
+        sort: true
+      },
+      {
+        dataField: 'FGM',
+        text: 'FGM',
+        sort: true
+      },
+      {
+        dataField: 'FGA',
+        text: 'FGA',
+        sort: true
+      },
+      {
+        dataField: 'FG_PCT',
+        text: 'FG%',
+        sort: true
+      }, 
+      {
+        dataField: 'FG3M',
+        text: '3PTM',
+        sort: true
+      },
+      {
+        dataField: 'FTM',
+        text: 'FTM',
+        sort: true
+      },
+      {
+        dataField: 'FTA',
+        text: 'FTA',
+        sort: true
+      },
+      {
+        dataField: 'FT_PCT',
+        text: 'FT%',
+        sort: true
+      },
+      {
+        dataField: 'REB',
+        text: 'REB',
+        sort: true
+      },
+      {
+        dataField: 'AST',
+        text: 'AST',
+        sort: true
+      },
+      {
+        dataField: 'STL',
+        text: 'STL',
+        sort: true
+      },
+      {
+        dataField: 'BLK',
+        text: 'BLK',
+        sort: true
+      },
+      {
+        dataField: 'TOV',
+        text: 'TOV',
+        sort: true
+      },
+    ]
+  }
 
-  /*useEffect(()=>{
-      async function fetchdata(){
-          axios.get('/predict/10')
-          .then(res => {
-              const games = res.data;
-              setData(games);
-          })
-      }
-      fetchdata();
-  },[])*/
- 
-  return (
-    <ul style={{textAlign:"center"}}>
-      <h2>Latest Predictions</h2>
-      {data.map(item => (
-        <div key={item.index}>
-            <h4>{item.vteam} at {item.hteam}</h4>
-            <p>{item.visitor_pred} to {item.home_pred}</p>
-        </div>
-      ))}
-    </ul>
-  );
+  componentDidMount() {
+    axios.get('/api/player')
+      .then(response => {
+        this.setState({
+          products: response.data
+        });
+      });
+  }
+  
+  render() {
+    return (
+      <div className="container" style={{ marginTop: 50 }}>
+        <BootstrapTable 
+        striped
+        hover
+        keyField='id' 
+        data={ this.state.products } 
+        columns={ this.state.columns } />
+      </div>
+    );
+  }
 }
- 
+
 export default Home;
