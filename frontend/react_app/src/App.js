e7492115e89e063eaeed180afedd44088e1df441
WillYip716 <william.yip.716@gmail.com>
2021-05-01 20:33:46 -0700

    compiler component for transfering league data

--- frontend/react_app/src/App.js
+++ frontend/react_app/src/App.js
@@ -6,6 +6,7 @@ import NavbarComp from './components/navbar';
 import Roster from './components/rosters';
 import Team from './components/team';
 import Ratings from './components/ratings';
+import LeagueCompiler from './components/leaguecompiler';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Rankings from './components/rankings';
 
@@ -20,6 +21,7 @@ function App() {
         <Route path="/team/:id" exact component={Team} />
         <Route path="/about" exact component={About} />
         <Route path="/ratings" exact component={Ratings} />
+        <Route path="/compile" exact component={LeagueCompiler} />
     </div>
   );
 }
---
+++ frontend/react_app/src/components/leaguecompiler.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { Form,Button } from "react-bootstrap";
+//import axios from 'axios';
+
+
+class LeagueCompiler extends Component {
+    state = {
+      textarea: "",
+    }
+
+    handleChange = (event) => {    
+        this.setState({
+            textarea: event.target.value,
+        });
+    }
+    
+    handleSubmit = (event) => {  
+        event.preventDefault(); 
+        let arr = [];
+        let outarr = [];
+        let reTeam =  new RegExp("([a-zA-Z .'0-9]*)(?=\\nPos)","g");
+        let rePlayers = new RegExp('(?<=\\n)([a-zA-Z .]*) (?=Atl|Bkn|Bos|Cha|Chi|Cle|Dal|Den|Det|GS|Hou|Ind|LAC|LAL|Mem|Mia|Mil|Min|NO|NY|OKC|Orl|Phi|Pho|Por|SA|Sac|Tor|Uta|Was)',"g");
+        arr = this.state.textarea.split("\n\n");
+        arr = arr.filter((i) => (i.indexOf("Pos")>-1));
+        for(let i = 0; i < arr.length; i++){
+            outarr.push({
+                'teamName': arr[i].match(reTeam)[0],
+                'players': arr[i].match(rePlayers)
+            });
+        }
+        console.log(outarr)
+    }
+
+    
+    
+    render() {
+      return (
+        <div className="container">
+            <Form onSubmit={this.handleSubmit} style={{textAlign:"center", margin:"auto"}}role="form">
+                <textarea style={{width: "80%",margin:"auto", height: "500px"}} value={this.state.textarea} onChange={this.handleChange} />
+                <br/>
+                <Button type="submit">Compile</Button>
+            </Form>
+        </div>
+        
+      );
+    }
+  }
+  
+  export default LeagueCompiler;
